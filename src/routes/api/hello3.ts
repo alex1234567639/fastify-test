230bf214c98dece267ae66175c45195d5cb9d678
alex1234567639 <alex@omniguider.com>
2024-06-05 15:49:29 +0800

    [fetaure]@fastify/auth實作

--- src/routes/api/hello3.ts
+++ src/routes/api/hello3.ts
@@ -1,45 +1,33 @@
-import { FastifyInstance, RouteShorthandOptions } from 'fastify';
+import { FastifyPluginAsync } from 'fastify'
 import fastifyAuth from '@fastify/auth';
-// import { verifyToken } from './../../handlers/auth'; // 根據你的文件結構調整相對路徑
+import { verifyToken } from './../../handlers/auth'; // 根據你的文件結構調整相對路徑
 
 interface NameParams {
   name: string;
 }
-const opts: RouteShorthandOptions = {
-  schema: {
-      params: {
-          type: 'object',
-          properties: {
-            name: { type: 'string' }
-          },
-          required: ['name']
-        },
-      response: {
-          200: {
-              type: 'object',
-              properties: {
-                  message: { type: 'string' }
-              }
-          }
-      }
-  }
-};
 
-export default async function (server: FastifyInstance) {
-  // 註冊 fastify-auth 插件
-  server.register(fastifyAuth);
-
-  // curl -X POST http://127.0.0.1:3000/hello3/alex
-  server.post<{ Params: NameParams }>('/hello3/:name', opts, async (request, reply) => {
+const workspaces: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  // curl -X POST http://127.0.0.1:3000/api/hello3/alex
+  fastify.post<{ Params: NameParams }>('/hello3/:name', async (request, reply) => {
       const name = request.params.name;
       return reply.status(200).send({ message: `Hello ${name}` });
   });
-  server.get('/public', async (request, reply) => {
+  fastify.get('/public', async (request, reply) => {
       return { message: 'hello3 public' };
   });
-  // server.get('/protected', {
-  //   preHandler: server.auth([verifyToken])
-  // }, async (request, reply) => {
-  //     return { message: 'hello3 protected' };
-  // });
+
+  fastify
+  .register(fastifyAuth)
+  .after(() => {
+    fastify.route({
+      method: 'GET',
+      url: '/protected',
+      preHandler: fastify.auth([verifyToken]),
+      handler: async (request, reply) => {
+        return { message: 'hello3 protected' };
+      }
+    })
+  })
 }
+
+export default workspaces
