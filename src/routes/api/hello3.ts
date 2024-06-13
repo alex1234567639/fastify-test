ce42b4eaab1052e12044b40d4faa3576b5dab46c
alex1234567639 <alex@omniguider.com>
2024-06-13 18:00:44 +0800

    [feature]新增QueryString example

--- src/routes/api/hello3.ts
+++ src/routes/api/hello3.ts
@@ -5,6 +5,10 @@ import { verifyToken } from './../../handlers/auth'; // 根據你的文件結構
 interface NameParams {
   name: string;
 }
+interface MyQueryString {
+  key01: string
+  key02: string
+}
 
 const workspaces: FastifyPluginAsync = async (fastify: FastifyInstance , opts): Promise<void> => {
   fastify.addHook('onReady', (done) => {
@@ -16,6 +20,11 @@ const workspaces: FastifyPluginAsync = async (fastify: FastifyInstance , opts): 
       const name = request.params.name;
       return reply.status(200).send({ message: `Hello ${name}` });
   });
+  // curl -X POST "http://127.0.0.1:3000/api/hello3/querystring?key01=aaa&key02=bbb"
+  fastify.post<{ Querystring: MyQueryString }>('/hello3/querystring', (request, reply) => {
+    const { key01, key02 } = request.query;
+    return reply.status(200).send({ message: `Hello ${key01} & ${key02}` });
+  })
   fastify.get('/public', async (request, reply) => {
       return { message: 'hello3 public' };
   });
