8949f69f0c982cb9b45003754700cc64d3f1884f
alex1234567639 <alex@omniguider.com>
2024-06-13 16:27:21 +0800

    [feature]新增lifecycle example

--- src/routes/hello.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from 'fastify'
-
-const hello: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/hello', async function (request, reply) {
-    return { message: 'Hello World' }
-  })
-}
-
-export default hello
---
+++ src/routes/lifecycle.ts
@@ -0,0 +1,44 @@
+import { FastifyPluginAsync } from 'fastify'
+
+const hello: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get('/life_cycle', async function (request, reply) {
+    return { message: 'Hello Lifecycle' }
+  })
+
+  fastify.addHook('onRequest', (request, reply, done) => {
+    console.log('onRequest hook')
+    done()
+  })
+
+  fastify.addHook('preParsing', (request, reply, payload, done) => {
+    console.log('preParsing hook')
+    done(null, payload)
+  })
+  
+  fastify.addHook('preValidation', (request, reply, done) => {
+    console.log('preValidation hook')
+    done()
+  })
+  
+  fastify.addHook('preHandler', (request, reply, done) => {
+    console.log('preHandler hook')
+    done()
+  })
+  
+  fastify.addHook('preSerialization', (request, reply, payload, done) => {
+    console.log('preSerialization hook')
+    done(null, payload)
+  })
+  
+  fastify.addHook('onSend', (request, reply, payload, done) => {
+    console.log('onSend hook')
+    done(null, payload)
+  })
+  
+  fastify.addHook('onResponse', (request, reply, done) => {
+    console.log('onResponse hook')
+    done()
+  })
+}
+
+export default hello
